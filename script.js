f37aba12fa8eb510f105da6ee0dd3298c685524e
Ishaan Jamdar <ishaan.jamdar@gmail.com>
2022-05-31 13:18:42 -0400

    Finished canvas

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-const container = document.querySelector('.container');
+const container = document.querySelector('.canvas');
 
 for (let i = 0; i < 256; i++) {
   let square = document.createElement('div');
   square.classList.add('grid-square');
+
+  square.addEventListener('mouseenter', () => {
+    square.classList.add('hovered');
+  });
+
   container.appendChild(square);
 }
