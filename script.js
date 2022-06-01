c02071dddb7f80b6e650b079086dfe9a6e28ff9a
Ishaan Jamdar <ishaan.jamdar@gmail.com>
2022-05-31 20:43:07 -0400

    Finished script and stylings

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const container = document.querySelector('.canvas');
+const input = document.querySelector('input');
+const canvas = document.querySelector('.canvas');
+const sizeText = document.querySelector('.size-text');
 
-for (let i = 0; i < 256; i++) {
-  let square = document.createElement('div');
-  square.classList.add('grid-square');
-
-  square.addEventListener('mouseenter', () => {
-    square.classList.add('hovered');
-  });
-
-  container.appendChild(square);
+function createCanvas(size) {
+  for (let i = 0; i < size ** 2; i++) {
+    let square = document.createElement('div');
+    square.style.height = `${512 / size}px`;
+    square.style.width = `${512 / size}px`;
+    canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  
+    square.addEventListener('mouseenter', () => {
+      square.classList.add('hovered');
+    });
+  
+    container.appendChild(square);
+  }
 }
+
+input.addEventListener('input', e => {
+  container.innerHTML = '';
+  sizeText.textContent = `Canvas Size: ${e.target.value}x${e.target.value}`
+  createCanvas(e.target.value);
+})
+
+createCanvas(16);
